97ccd44c330fb1e3372967cd9b9818af836769d5
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2023-08-09 14:17:08 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+// connected to database
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/Issue-Tracker`);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+
+db.once('open', function(){
+    console.log("Connected to Database :: mongodb");
+});
+
+module.exports = db;
