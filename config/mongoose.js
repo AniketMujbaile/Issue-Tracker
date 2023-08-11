b6b53b3641ef1d73433e2f56c8808d3b9fcdbcf6
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2023-08-11 16:11:12 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,16 @@
- const mongoose = require("mongoose");
+ // connected to database
+const mongoose = require('mongoose');
 
-// Connection to mongodb.
-mongoose.connect("mongodb+srv://aniketmujbaile1:OTAxMTI3MTU5NQ==@cluster0.ittt685.mongodb.net/?retryWrites=true&w=majority"); 
+mongoose.connect(`mongodb+srv://aniketmujbaile1:uvwLUuTtCRGsFZvp@cluster0.zmb9bya.mongodb.net/?retryWrites=true&w=majority`);
 
-const db = mongoose.connection; // db store the connection
+const db = mongoose.connection;
 
-// cheacking the connection
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("DATABASE connection is Established");
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+
+db.once('open', function(){
+    console.log("Connected to Database :: mongodb");
 });
 
-// exporting the connection.
 module.exports = db;
  
 
